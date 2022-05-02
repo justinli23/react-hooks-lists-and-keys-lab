0214787e796eb9d32ea65067c77252c90499fdc3
Justin Li <justinlit23@gmail.com>
2022-05-02 15:28:15 -0700

    commit tree fb885f7e5a074b716fc0be678211644a4125b4c1

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const jsxLinks = links.map((link) => {
+    return <a href="#{link}" key={link}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+      {jsxLinks}
+  </nav>;
 }
 
 export default NavBar;
